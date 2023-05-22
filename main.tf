9fed4ca16887693abb996bb47ff0fce7f6b68b1f
ampacheco <ampacheco@users.noreply.github.com>
2023-05-22 00:18:23 -0400

    default-cpu-and-memory

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ resource "azurerm_container_group" "example" {
   container {
     name   = "dvwa-on-aci"
     image  = "vulnerables/web-dvwa:latest"
-    cpu    = "0.5"
-    memory = "1.5"
+    # cpu    = "0.5"
+    # memory = "1.5"
 
     ports {
       port     = 80
